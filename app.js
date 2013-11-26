553733289a95944c34fac118243866abb025b164
Vanderson Martins do Rosario <vanderson@vanderson-PC>
2013-11-26 20:40:12 -0200

    Comments system

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.post('/poll/add', poll.add);
+app.get('/poll/comment/:id/', poll.comment);
 app.get('/poll/remove/:id', poll.remove);
 app.get('/polls', poll.list);
 
