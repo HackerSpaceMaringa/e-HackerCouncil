e3409d57cf3d3f2ae5254cab6dabdebea5e4c286
Vanderson Martins do Rosario <vanderson@vanderson-PC>
2013-11-26 16:20:35 -0200

    MongoDB working

--- public/javascript/pollDAO.js
+++ public/javascript/pollDAO.js
@@ -2,26 +2,33 @@
  * PollDAO is responsable to manage polls in the database.
  */
 
-var pollDAO = function {
-  /* Insert a poll in DB. Expecting a to be a poll object.
-   * PollObj:
-   *  a.title
-   *  a.description
-   *  a.date
-   *  a.owner   <-- Is a user name
-   *  */
-  function insert(a)  {
+exports.pollDAO = function(mongoose) {
+  var pollSchema = new mongoose.Schema({
+    id: Number,
+    title: { type: String },
+    description: String,
+    date: Number,
+  });
+
+  var poll = mongoose.model('poll', pollSchema);
+
+  // Insert a poll in DB. Expecting a to be a poll object.
+  this.insert = function(a) {
+    var newPoll = new poll(a);
+    newPoll.save(function(err, newPoll){
+      if (err) return console.error(err);
+      console.dir(newPoll);
+    });
+  }
+
+  // Remove a poll with the same ID; 
+  this.remove = function(id) {
     //TODO
   }
 
-  /* Remove a poll with the same ID; */
-  function remove(id) {
-    //TODO
-  }
-
-  /* List the last n polls createds.
-   * n = number of polls to list. */
-  function list(n) {
+  // List the last n polls createds.
+  // n = number of polls to list. */
+  this.list = function(n) {
     //TODO
   }
 }
