ec55baa07082c0348ef7923781b72c480bd39da8
Vanderson M. do Rosario <vandersonmr2@gmail.com>
2013-11-27 13:51:43 -0200

    Init vote system

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,12 @@
  */
 
 exports.logged = function(req, res) {
+   var user = {
+      name: req.user.displayName,
+      email: req.user.emails[0].value,
+      level: 0
+   };
+   userDAO.dontExist(user,userDAO.insert);
    res.redirect('/polls');
 }
 
